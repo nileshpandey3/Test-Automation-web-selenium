f02f7c5d85cec7e5bb754553c9fcbcdd379aaf55
Nilesh <nileshpandey3@gmail.com>
2018-05-15 23:37:34 -0700

    first signin page verification test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import sun.jvm.hotspot.utilities.Assert;
+import static java.time.Duration.ofMillis;
+import static java.time.Duration.ofMinutes;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+
+    @Test
+    public void signInTest() {
+        FirefoxDriver driver = new FirefoxDriver();
+        driver.get("https://www.marcus.com/us/en");
+        driver.findElementByCssSelector(".Navigation__login").click();
+        String expectedUrl = "https://www.marcus.com/us/en/login";
+        driver.get(expectedUrl);
+        assertEquals(expectedUrl,driver.getCurrentUrl());
+
+
+
     }
 }
